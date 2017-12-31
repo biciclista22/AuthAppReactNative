a9e673b7c884e5a05a7daa2a46ec5a244cae599a
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-12-31 01:25:48 -0500

    Set up app, using components from prior project, initial Firebase set up, about to create form

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCUXo9Zp-cFXSR-eVbQSfXS8r4uljFvZSc',
+      authDomain: 'let-s-auth-this.firebaseapp.com',
+      databaseURL: 'https://let-s-auth-this.firebaseio.com',
+      projectId: 'let-s-auth-this',
+      storageBucket: 'let-s-auth-this.appspot.com',
+      messagingSenderId: '27626409458'
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication"/>
+        <LoginForm />
+      </View>
+    );
+  };
+}
+
+export default App;
